07ae4b5f5f1ed969990a64008ea2e05c1f15621b
Mibin Thomas <mibin@bosq.ae>
2026-06-19 15:32:54 +0400

    updated bulk upload ui

--- src/components/products/bulk-upload-modal.tsx
+++ src/components/products/bulk-upload-modal.tsx
@@ -19,7 +19,8 @@ import {
   FileCheck2,
   FileWarning,
   CheckCircle,
-  HelpCircle
+  HelpCircle,
+  Info
 } from "lucide-react"
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
